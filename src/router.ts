257f81b0720a5e0a56ab621e9b5a2b1db638da7f
Callum Macdonald <git@callum-macdonald.com>
2024-05-22 19:01:02 +0200

    Store tr username from url.

--- src/router.ts
+++ src/router.ts
@@ -1,81 +1,16 @@
 import { nip19 } from "nostr-tools";
 
-export const getPublicKeyFromUrl = ({
+export const getTrustrootsUsernameFromLocation = ({
   location = globalThis.document.location,
 }: {
   location?: Location;
-} = {}): {
-  publicKey?: string;
-  view?: {
-    zoom: number;
-    lat: number;
-    lng: number;
-  };
-} => {
+} = {}): string => {
   const { hash } = location;
-  if (!hash) return {};
-  const params = hash.split("/"); // #/{pubkey}/{zoom}/{lat}/{lng}
-  console.log(hash, params);
-  let publicKey;
-  if (params[1].startsWith("npub")) {
-    const { type, data } = nip19.decode(params[1]);
-    if (type === "npub") publicKey = data as string;
+  const params = hash.split("="); // /#user={user}
+  if (typeof params[1] === "string") {
+    const username = params[1];
+    const sanitisedUsername = encodeURIComponent(username);
+    return sanitisedUsername;
   }
-
-  let view;
-  const zoomParam = Number.parseInt(params[2]);
-  const latParam = Number.parseFloat(params[3]);
-  const lngParam = Number.parseFloat(params[4]);
-  if (
-    !Number.isNaN(zoomParam) &&
-    zoomParam > 0 &&
-    zoomParam < 18 &&
-    !Number.isNaN(lngParam) &&
-    !Number.isNaN(lngParam)
-  )
-    view = {
-      zoom: zoomParam,
-      lat: latParam,
-      lng: lngParam,
-    };
-
-  return {
-    publicKey,
-    view,
-  };
-};
-
-export const getUrlFromNpubPublicKey = ({
-  npubPublicKey,
-  location = globalThis.document.location,
-  view,
-}: {
-  npubPublicKey: string;
-  location?: Location;
-  view?: {
-    zoom: number;
-    lat: number;
-    lng: number;
-  };
-}): string => {
-  const { origin, pathname } = location;
-  let yourUrl = origin + pathname + "#/" + npubPublicKey;
-  if (view) yourUrl += `/${view.zoom}/${view.lat}/${view.lng}`;
-  return yourUrl;
-};
-
-export const getUrlFromPublicKeyAndView = ({
-  publicKey,
-  view,
-}: {
-  publicKey: string;
-  view?: {
-    zoom: number;
-    lat: number;
-    lng: number;
-  };
-}): string => {
-  const npubPublicKey = nip19.npubEncode(publicKey);
-  const yourUrl = getUrlFromNpubPublicKey({ npubPublicKey, view });
-  return yourUrl;
+  return "";
 };
