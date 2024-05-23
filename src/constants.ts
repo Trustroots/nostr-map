0dd644370f7df4e022b5d366c5c613c5aac9c09b
Callum Macdonald <git@callum-macdonald.com>
2024-05-23 13:10:43 +0200

    Fix default relays for dev

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,7 @@ export const PRIVATE_KEY_STORAGE_KEY = "__nostrPrivateKey" as const;
 export const RELAYS_STORAGE_KEY = "__nostrRelays" as const;
 export const PLUS_CODE_TAG_KEY = "l" as const;
 export const MAP_NOTE_KIND = 397 as Kind;
-export const DEFAULT_RELAYS = [
-  "wss://nostr.massmux.com",
-  "wss://public.nostr.swissrouting.com",
-  "wss://nostr.slothy.win",
-];
+export const DEFAULT_RELAYS = ["wss://nos.lol"];
 export const PANEL_CONTAINER_ID = "panelID";
 export const BADGE_CONTAINER_ID = "badge";
 export const CURRENT_PUBLIC_KEY_ID = "current_public_key";
