bc33b1d8a44cbd69620804ce6973c9dbb89367de
Callum Macdonald <git@callum-macdonald.com>
2024-05-25 21:13:38 +0200

    prod relays

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,11 @@ export const PRIVATE_KEY_STORAGE_KEY = "__nostrPrivateKey" as const;
 export const RELAYS_STORAGE_KEY = "__nostrRelays" as const;
 export const PLUS_CODE_TAG_KEY = "l" as const;
 export const MAP_NOTE_KIND = 397 as Kind;
-export const DEFAULT_RELAYS = ["wss://nos.lol"];
+export const DEFAULT_RELAYS = [
+  "wss://relay.primal.net",
+  "wss://relay.damus.io",
+  "wss://nostr.manasiwibi.com",
+];
 export const PANEL_CONTAINER_ID = "panelID";
 export const BADGE_CONTAINER_ID = "badge";
 export const CONTENT_MINIMUM_LENGTH = 3;
